2585332eab12fc3a714c0236a33816d83bdf98fb
Jerin Prakash <jerinprakash892@gmail.com>
2026-01-13 18:27:51 +0530

    added the code

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 public class SumOfNaturalNumbers {
 
     // TODO: Create the method public static int NumberSum(int n)
     // It should calculate and return the sum of first n natural numbers
+	public static int NumberSum(int n){
+
+		int sum=n*(n+1)/2;
+
+		return sum;
+	}
+
+	public static void main(String[] args){
+
+		Scanner sc= new Scanner(System.in);
+
+		int n= sc.nextInt();
+
+		int sum= NumberSum(n);
+
+		System.out.println(sum);
+
+	}
 
 
         // Inside main method
